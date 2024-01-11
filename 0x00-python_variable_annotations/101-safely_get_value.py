ef7df15a5685f7d6e328b4cda81ecfe35e2702bd
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-11 17:29:24 +0300

    corrected pycode style

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,9 @@ from typing import Mapping, Any, TypeVar, Union
 T = TypeVar("T")
 Defa = Union[T, None]
 
-def safely_get_value(dct: Mapping, key: Any, default: Defa = None) -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping, key: Any, default: Defa = None
+                     ) -> Union[Any, T]:
     """This function returns the key of dict if available"""
     if key in dct:
         return dct[key]
