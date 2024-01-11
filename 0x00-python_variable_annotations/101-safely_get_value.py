7b5c69253bae9c14cdd0d3d72cc413466e54eea2
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-11 10:54:17 +0300

    validating my files types using mypy package

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Type annotation in dict object"""
+from typing import Mapping, Any, TypeVar, Union
+
+
+T = TypeVar("T")
+Defa = Union[T, None]
+
+def safely_get_value(dct: Mapping, key: Any, default: Defa = None) -> Union[Any, T]:
+    """This function returns the key of dict if available"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
