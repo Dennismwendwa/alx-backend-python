1b0b2b73bb3b11d5fdad862563d8053d2807e702
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-11 08:44:05 +0300

    basic annotations

--- 1-concat.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""This function concat two strings"""
-
-
-def concat(str1: str, str2: str) -> str:
-    """This function joins string variables"""
-    return f"{str1}{str2}"
--- 3-to_str.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""This function return string representation of float"""
-
-
-def to_str(n: float) -> str:
-    """This function convents float into string"""
-    return str(n)
