c96d4e2d115e020634c59e2b64240a68129a0822
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-11 17:30:57 +0300

    corrected pycode style

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k:str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """This function squares int inputs"""
     return k, v ** 2
