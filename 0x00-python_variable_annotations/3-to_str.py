6d30926f39b640ecbe4cd2d0d9e2753af822a557
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-11 09:32:48 +0300

    basic annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This function return string representation of float"""
+
+
+def to_str(n: float) -> str:
+    """This function convents float into string"""
+    return str(n)
