90a938996b56ed3b43ec8514fd8bf6e0b043fc24
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-15 21:16:00 +0300

    awaiting tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""This function calls asyncio"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Function that returns an asyncio.Task for wait_random"""
+    return asyncio.create_task(wait_random(max_delay))
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Asynchronous routine that spawns task_wait_random n times"""
+
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    completed = []
+
+    for task in asyncio.as_completed(tasks):
+        completed.append(await task)
+
+    return completed
