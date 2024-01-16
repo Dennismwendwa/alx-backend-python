15a8a9653d5fad86e11362ca29e9a6ff34c38d71
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-17 01:54:22 +0300

    Run time for four parallel comprehensions

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Run time for four parallel comprehensions"""
-import asyncio
-import time
+from asyncio import gather
+from time import time
 from typing import List
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -11,9 +11,8 @@ async def measure_runtime() -> float:
     Asynchronous coroutine that measures the total runtime for executing
     async_comprehension four times in parallel.
     """
-    start_time = time.time()
-
-    await asyncio.gather(
+    start_time = time()
+    await gather(
         *(async_comprehension() for _ in range(4))
     )
-    return time.time() - start_time
+    return time() - start_time
