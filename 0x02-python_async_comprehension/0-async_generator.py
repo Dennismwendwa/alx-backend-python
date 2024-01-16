5c85ac522ebe2dc1b2a464dc9a49c588dda6b126
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-17 00:51:12 +0300

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Async generator function"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Asynchronous coroutine that loops 10 times,
+    each time asynchronously waiting 1 second
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
