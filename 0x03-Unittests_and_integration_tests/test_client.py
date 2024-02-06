7cbe859bc32df303511b48a0e59fe878a0ccc0ff
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-06 05:11:43 +0300

    added intergarion tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,14 +44,12 @@ class TestGithubOrgClient(TestCase):
             'repos_url': "https://api.github.com/users/google/repos",
             'repos': [
                 {
-                    "id": 7697149,
+                    "id": 4444,
                     "name": "The river between",
-                    "url": "https://api.github.com/repos/google/episodes.dart",
                 },
                 {
-                    "id": 8566972,
+                    "id": 4444,
                     "name": "Fwaru",
-                    "url": "https://api.github.com/repos/google/kratu",
                 },
             ]
         }
@@ -68,3 +66,60 @@ class TestGithubOrgClient(TestCase):
             )
             pub_repos_call.assert_called_once()
         mock_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "bsd-3-clause"}}, "bsd-3-clause", True),
+        ({"license": {"key": "bsl-1-.0"}}, "bsd-3-clause", False),
+    ])
+    def test_has_license(self, repo: Dict, key: str, expected: bool) -> None:
+        """This method is testing has license function"""
+        github_c = GithubOrgClient("google")
+        client_license = github_c.has_license(repo, key)
+        self.assertEqual(client_license, expected)
+
+
+@parameterized_class([
+    {
+        "org_payload": TEST_PAYLOAD[0][0],
+        "repos_payload": TEST_PAYLOAD[0][1],
+        "expected_repos": TEST_PAYLOAD[0][2],
+        "apache2_repos": TEST_PAYLOAD[0][3],
+    },
+])
+class TestIntegrationGithubOrgClient(TestCase):
+    """Testing integration of https requests"""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """This method sets up all common features for the test methods"""
+        comm_urls = {
+            "https://api.github.com/orgs/google": cls.org_payload,
+            "https://api.github.com/orgs/google/repos": cls.repos_payload,
+        }
+
+        def send_requests(url):
+            """send requests, (not true requests)"""
+            return Mock(**{"json.return_value": comm_urls[url]}
+                        ) if url in comm_urls else HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=send_requests)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """Testing the public method"""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos
+        )
+
+    def test_public_repos_license(self) -> None:
+        """Testing if public repo has license"""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Cleaning test data"""
+        cls.get_patcher.stop()
